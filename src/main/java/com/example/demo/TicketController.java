f8956f5998fe0b83212a198e4b4470e3ab767518
ChamaniWanniarachchi <chamani.20230441@iit.ac.lk>
2024-12-10 15:48:30 +0530

    Fifth Commit

--- src/main/java/com/example/demo/TicketController.java
+++ src/main/java/com/example/demo/TicketController.java
@@ -7,6 +7,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class TicketController {
     @Autowired
